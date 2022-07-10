55ffc14e8674177a873e520f1ef37e3a77a86da2
yuri014 <yuriflores2012@gmail.com>
2022-07-09 21:52:45 -0300

    feat: creating header

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<script setup></script>
+
+<template>
+  <header>
+    <div class="container">
+      <p>
+        Formulário <br />
+        para compras de
+      </p>
+      <strong>Pacote de adesivos</strong>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  background-color: #2f3676;
+  width: 100vw;
+  padding: 2rem;
+  margin-bottom: 2rem;
+
+  .container {
+    width: 90vw;
+
+    p {
+      font-size: 2rem;
+      font-weight: 300;
+      color: #f1e7e8;
+    }
+
+    strong {
+      font-size: 2rem;
+      color: #fff;
+    }
+  }
+}
+</style>
