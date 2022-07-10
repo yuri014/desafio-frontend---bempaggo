f7e60b7f89620094627a94a1c7d37c695e353635
yuri014 <yuriflores2012@gmail.com>
2022-07-09 21:53:08 -0300

    feat: creating form wizard

---
+++ src/components/Form.vue
@@ -0,0 +1,95 @@
+<script>
+export default {
+  data: () => ({
+    stickers: {
+      react: {
+        count: 0,
+      },
+      vue: {
+        count: 0,
+      },
+      angular: {
+        count: 0,
+      },
+    },
+  }),
+  methods: {
+    capitalize: (word) => word.charAt(0).toUpperCase() + word.slice(1),
+    incrementCount(sticker) {
+      this.stickers[sticker].count++;
+    },
+    decrementCount(sticker) {
+      if (this.stickers[sticker].count !== 0) {
+        this.stickers[sticker].count--;
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <form class="container">
+    <p>Quais adesivos:</p>
+    <div class="stickers__container">
+      <div class="sticker" v-for="sticker in Object.keys(stickers)">
+        <label :for="sticker">{{ capitalize(sticker) }}</label>
+        <div class="sticker-buttons">
+          <button type="button" @click="decrementCount(sticker)">-</button>
+          <input type="number" :name="sticker" :id="sticker" :value="stickers[sticker].count" />
+          <button type="button" @click="incrementCount(sticker)">+</button>
+        </div>
+      </div>
+    </div>
+    <textarea name="obs" id="obs" cols="30" rows="10"></textarea>
+  </form>
+</template>
+
+<style lang="scss">
+p {
+  font-size: 1.6rem;
+  font-weight: bold;
+}
+
+.stickers__container {
+  margin: 2rem 0;
+
+  .sticker {
+    margin: 1rem 0;
+    width: 20rem;
+    display: grid;
+    grid-template-columns: 30% 30%;
+    align-items: center;
+    gap: 1rem;
+
+    label {
+      font-size: 1.4rem;
+    }
+
+    .sticker-buttons {
+      display: flex;
+      border-radius: 4px;
+      box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+
+      button {
+        border: none;
+        background-color: transparent;
+        color: #2f3676;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 1.4rem;
+        font-weight: bold;
+        width: 2.5rem;
+        height: 2.5rem;
+      }
+
+      input {
+        border: none;
+        width: 2.5rem;
+        height: 2.5rem;
+        text-align: center;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/FormWizard.vue
@@ -0,0 +1,223 @@
+<script>
+import Form from "./Form.vue";
+export default {
+    data: () => ({
+        steps: ["Pedido", "Pagamento"],
+        currentStep: 0,
+    }),
+    computed: {
+        stepperProgress() {
+            const total = 100;
+            return `${(total / (this.steps.length - 1)) * this.currentStep}%`;
+        },
+        currentStepValue() {
+            return this.steps.at(this.currentStep);
+        },
+    },
+    components: { Form }
+};
+</script>
+
+<template>
+  <div class="wrapper-stepper container">
+    <div class="stepper">
+      <div class="stepper-progress">
+        <div class="stepper-progress-bar" :style="'width:' + stepperProgress"></div>
+      </div>
+      <div
+        class="stepper-item"
+        :class="{ current: currentStepValue == item, success: currentStep > steps.indexOf(item) }"
+        v-for="item in steps"
+        :key="item"
+      >
+        <div class="stepper-item-counter">
+          <img
+            class="icon-success"
+            src="https://www.seekpng.com/png/full/1-10353_check-mark-green-png-green-check-mark-svg.png"
+            alt=""
+          />
+          <span class="number">
+            {{ steps.indexOf(item) + 1 }}
+          </span>
+        </div>
+        <span class="stepper-item-title"> {{ item }} </span>
+      </div>
+    </div>
+
+    <div class="stepper-content" v-for="item in steps" :key="item">
+      <div class="stepper-pane" v-if="currentStepValue == item">
+        <Form />
+      </div>
+    </div>
+
+    <div class="controls">
+      <button class="btn" @click="currentStep--" :disabled="currentStep == 0">Anterior</button>
+      <button class="btn btn--green-1" @click="currentStep++" :disabled="currentStep == 1">
+        Seguinte
+      </button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper-stepper {
+  background-color: #fff;
+  padding: 2rem 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  border-radius: 32px;
+  box-shadow: 0 8px 12px rgba($color: #000000, $alpha: 0.09);
+  width: 90vw;
+}
+
+.stepper {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 60%;
+  position: relative;
+  z-index: 0;
+  margin-bottom: 24px;
+
+  &-progress {
+    position: absolute;
+    background-color: #c5c5c5;
+    height: 2px;
+    z-index: -1;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+
+    &-bar {
+      position: absolute;
+      left: 0;
+      height: 100%;
+      width: 0%;
+      background-color: #2f3676;
+      transition: all 500ms ease;
+    }
+  }
+}
+
+.stepper-item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #c5c5c5;
+  transition: all 500ms ease;
+
+  &-counter {
+    height: 4rem;
+    width: 4rem;
+    display: grid;
+    place-items: center;
+    background-color: #fff;
+    border-radius: 100%;
+    border: 2px solid #c5c5c5;
+    position: relative;
+
+    .icon-success {
+      position: absolute;
+      opacity: 0;
+      transform: scale(0);
+      width: 1.2rem;
+      transition: all 500ms ease;
+    }
+
+    .number {
+      font-size: 22px;
+      transition: all 500ms ease;
+    }
+  }
+
+  &-title {
+    position: absolute;
+    font-size: 14px;
+    bottom: -24px;
+  }
+}
+
+.stepper-item.success {
+  .stepper-item-counter {
+    border-color: #2f3676;
+    background-color: #485091;
+    color: #fff;
+    font-weight: 600;
+
+    .icon-success {
+      opacity: 1;
+      transform: scale(1);
+    }
+
+    .number {
+      opacity: 0;
+      transform: scale(0);
+    }
+  }
+
+  .stepper-item-title {
+    color: #2f3676;
+  }
+}
+
+.stepper-item.current {
+  .stepper-item-counter {
+    border-color: #2f3676;
+    background-color: #2f3676;
+    color: #fff;
+    font-weight: 600;
+  }
+
+  .stepper-item-title {
+    color: #818181;
+  }
+}
+
+.stepper-content {
+  width: 100%;
+  padding: 0 2rem;
+}
+
+.stepper-pane {
+  color: #333;
+  min-height: 20rem;
+  margin: 4rem 0;
+}
+
+.controls {
+  display: flex;
+}
+
+.btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 6px 16px;
+  border: 1px solid;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  line-height: 1.5;
+  transition: all 150ms;
+  border-radius: 4px;
+  width: fit-content;
+  font-size: 0.75rem;
+  color: #333;
+  background-color: #f0f0f0;
+  border-color: #f0f0f0;
+
+  &:disabled {
+    opacity: 0.5;
+    pointer-events: none;
+  }
+
+  &--green-1 {
+    background-color: #2f3676;
+    border-color: #2f3676;
+    color: #fff;
+    margin-left: auto;
+  }
+}
+</style>
