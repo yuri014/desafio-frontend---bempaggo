ec8294da231a066366d03b3f038cbf02fc077481
yuri014 <yuriflores2012@gmail.com>
2022-07-09 21:53:24 -0300

    feat: add form and header

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import Header from './components/Header.vue';
+import FormWizard from './components/FormWizard.vue';
+</script>
+
+<template>
+  <Header />
+  <FormWizard />
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap');
+:root {
+  font-size: 60%;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: 'Roboto', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #eaedf0;
+}
+
+body {
+  background-color: #f1f0f0;
+}
+
+.container {
+  max-width: 1378px;
+  margin: 0 auto;
+}
+</style>
