a6e45704ae2ee8b4871397e137448ad80e79933c
yuri014 <yuriflores2012@gmail.com>
2022-07-11 20:42:18 -0300

    feat: add waves and icons

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,25 +1,36 @@
-<script setup></script>
+<script setup>
+import PackageIcon from '../assets/package.svg';
+</script>
 
 <template>
   <header>
     <div class="container">
-      <p>
-        Formulário <br />
-        para compras de
-      </p>
-      <strong>Pacote de adesivos</strong>
+      <div>
+        <p>
+          Formulário <br />
+          para compras de
+        </p>
+        <strong>Pacote de adesivos</strong>
+      </div>
+      <div class="package-icon"><PackageIcon /></div>
     </div>
   </header>
 </template>
 
 <style lang="scss" scoped>
 header {
-  background-color: $brand-color;
   width: 100vw;
-  padding: 2rem;
-  margin-bottom: 2rem;
+  height: 18rem;
+  background-image: url('../assets/wave.svg');
+  background-repeat: repeat-x;
+  background-size: cover;
+  background-position: center center;
+  position: relative;
 
   .container {
+    padding: 2rem;
+    padding-bottom: 0;
+    display: flex;
     p {
       font-size: 2rem;
       font-weight: 300;
@@ -29,6 +40,14 @@ header {
     strong {
       font-size: 2rem;
       color: $pure-white;
+    }
+
+    svg {
+      position: absolute;
+      transform: scale(0.6) rotateX(35deg);
+      fill: #191647;
+      margin-top: -8rem;
+      margin-left: -5rem;
     }
   }
 }
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), svgLoader()],
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: `
           @import "./src/styles/scss/_variables.scss";
-        `
-      }
-    }
-  }
+        `,
+      },
+    },
+  },
 });
